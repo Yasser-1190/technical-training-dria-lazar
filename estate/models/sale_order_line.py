ebfb9cc254e4d01daa881422928a3a62a39c0637
Alexandru Lazar <alexandru.lazar@helb-prigogine.be>
2023-01-04 19:32:58 +0100

    maj employee assigned

--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -3,3 +3,4 @@ from odoo import fields, models, Command
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     training_date = fields.Char(string="Training Date")
+    employee = fields.Char(string="Assigned Employee")
