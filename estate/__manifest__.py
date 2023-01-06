388ddadd467195e15ee341853e4fd97d863bea8e
Alexandru Lazar <79793893+alazar-HELB@users.noreply.github.com>
2023-01-06 22:11:47 +0100

    removing dependency attempt

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,6 +7,5 @@
     'category': 'Uncategorized',
     'version': '15.0.0.1',
     'depends': ['base', 'sale_management', 'calendar', 'hr'],
-    'data': ["views/res_groups.xml", "views/res_partner.xml", "views/sale_order.xml"],
-    'depends': ['sale.order.line']
+    'data': ["views/res_groups.xml", "views/res_partner.xml", "views/sale_order.xml"]
 }
