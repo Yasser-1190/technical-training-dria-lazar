7c29f2d13cc7a0afdfd6850b84233366461d1394
Alexandru Lazar <79793893+alazar-HELB@users.noreply.github.com>
2023-01-06 22:09:50 +0100

    removing attribute

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -4,16 +4,12 @@ from odoo.exceptions import ValidationError
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    @api.constrains('partner_id', 'amount_total')
     def check_max_sale_order_amount(self):
         for order in self:
             if order.partner_id.max_sale_order_amount and order.amount_total > order.partner_id.max_sale_order_amount:
                 raise ValidationError(_('The sale order amount is greater than the maximum allowed amount for the partner.'))
 
-    approval_status = fields.Selection(
-    related='approval_id.state',
-    string='Approval Status',
-    )
+
 
     approval_id = fields.Many2one(
         'sale.order.approval',
