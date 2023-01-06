8a7cd7489093d26e5bc48f947708f2422bbddb50
Alexandru Lazar <alexandru.lazar@helb-prigogine.be>
2023-01-06 21:07:33 +0100

    sale order approve retry

--- estate/models/sale_order_approve.py
+++ estate/models/sale_order_approve.py
@@ -18,30 +18,34 @@ class SaleOrderApprove(models.Model):
     )
     level = fields.Selection(
         string='Level',
+        # level selection
         selection=[
             ('1', 'Level 1'),
             ('2', 'Level 2'),
             ('3', 'Level 3'),
-            # add more levels as needed
         ],
         required=True,
         default='1',
     )
     
-    def transition_approve(self):
-        if self.amount < 500:
-            return True
-        return False
+    
     
     def create_workflow(self):
         workflow = Workflow(self._name)
-        # define the states
+        # states
         workflow.add_state('pending_level_1_approval', 'Pending Level 1 Approval')
         workflow.add_state('pending_level_2_approval', 'Pending Level 2 Approval')
         workflow.add_state('approved', 'Approved')
-        # define the transitions
+
+        def transition_approve(self, cr, uid, ids, context=None):
+            for approval in self.browse(cr, uid, ids, context=context):
+                if approval.amount < 500:
+                    return True
+            return False
+
+        # transitions
         workflow.add_transition('approve', 'pending_level_1_approval', 'approved', condition=transition_approve)
         workflow.add_transition('reject', 'pending_level_1_approval', 'rejected')
         workflow.add_transition('escalate', 'pending_level_1_approval', 'pending_level_2_approval')
-        # add more transitions as needed
         return workflow
+
