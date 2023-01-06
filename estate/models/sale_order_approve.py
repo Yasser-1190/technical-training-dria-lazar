371faed4fbfdaebec42ae5fac61809c839062c6b
Alexandru Lazar <79793893+alazar-HELB@users.noreply.github.com>
2023-01-06 22:22:49 +0100

    retry

--- estate/models/sale_order_approve.py
+++ estate/models/sale_order_approve.py
@@ -2,8 +2,8 @@ from odoo import api, models, fields
 from odoo.exceptions import ValidationError
 
 class SaleOrderApprove(models.Model):
+    _inherit = 'sale.order'
     _name = 'sale.order.approval'
-    _inherit = 'sale.order.line'
     _description = 'Sale Order Approval'
 
     state = fields.Selection(
--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -4,3 +4,7 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     training_date = fields.Char(string="Training Date")
     employee = fields.Char(string="Assigned Employee")
+    approval_status = fields.Selection(
+    related='approval_id.state',
+    string='Approval Status',
+    )
