5dcd7829c6ec92a97c0575fa357f3025038e1975
Yasser-1190 <63496085+Yasser-1190@users.noreply.github.com>
2023-01-06 20:29:09 +0100

    Update sale_order_approve.py

--- estate/models/sale_order_approve.py
+++ estate/models/sale_order_approve.py
@@ -2,8 +2,8 @@ from odoo import api, models, fields
 from odoo.exceptions import ValidationError
 
 class SaleOrderApprove(models.Model):
-    _inherit = 'sale.order.line'
     _name = 'sale.order.approval'
+    _inherit = 'sale.order.line'
     _description = 'Sale Order Approval'
 
     sale_order_id = fields.Many2one(
@@ -27,3 +27,21 @@ class SaleOrderApprove(models.Model):
         required=True,
         default='1',
     )
+    
+    def transition_approve(self):
+        if self.amount < 500:
+            return True
+        return False
+    
+    def create_workflow(self):
+        workflow = Workflow(self._name)
+        # define the states
+        workflow.add_state('pending_level_1_approval', 'Pending Level 1 Approval')
+        workflow.add_state('pending_level_2_approval', 'Pending Level 2 Approval')
+        workflow.add_state('approved', 'Approved')
+        # define the transitions
+        workflow.add_transition('approve', 'pending_level_1_approval', 'approved', condition=transition_approve)
+        workflow.add_transition('reject', 'pending_level_1_approval', 'rejected')
+        workflow.add_transition('escalate', 'pending_level_1_approval', 'pending_level_2_approval')
+        # add more transitions as needed
+        return workflow
