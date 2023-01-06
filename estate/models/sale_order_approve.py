02d966a63a0133e59c3f3e35102846dc821e2cf8
Alexandru Lazar <alexandru.lazar@helb-prigogine.be>
2023-01-06 20:25:35 +0100

    retry for approve model

--- estate/models/sale_order_approve.py
+++ estate/models/sale_order_approve.py
@@ -3,4 +3,27 @@ from odoo.exceptions import ValidationError
 
 class SaleOrderApprove(models.Model):
     _inherit = 'sale.order.line'
-    
+    _name = 'sale.order.approval'
+    _description = 'Sale Order Approval'
+
+    sale_order_id = fields.Many2one(
+        'sale.order',
+        string='Sale Order',
+        required=True,
+        ondelete='cascade',
+    )
+    amount = fields.Float(
+        string='Amount',
+        required=True,
+    )
+    level = fields.Selection(
+        string='Level',
+        selection=[
+            ('1', 'Level 1'),
+            ('2', 'Level 2'),
+            ('3', 'Level 3'),
+            # add more levels as needed
+        ],
+        required=True,
+        default='1',
+    )
