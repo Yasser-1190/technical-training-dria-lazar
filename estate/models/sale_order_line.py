e08dac20c837a2c3bc3f5267d8b1ee56eaefd7df
Alexandru Lazar <79793893+alazar-HELB@users.noreply.github.com>
2023-01-08 22:48:25 +0100

    retry

--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -2,7 +2,7 @@ from odoo import fields, models
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
-    training_date = fields.datetime(string="Training Date", year=2023, month=1, day= 1)
+    training_date = fields.datetime(year=2023, month=1, day= 1)
     employee = fields.Char(string="Assigned Employee")
     approval_status = fields.Selection(
     related='approval_id.state',
