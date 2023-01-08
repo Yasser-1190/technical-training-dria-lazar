16306fe98fc15cf33dc4c94e596343548d7a3098
Alexandru Lazar <79793893+alazar-HELB@users.noreply.github.com>
2023-01-08 22:38:27 +0100

    update sales order line field

--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -2,7 +2,7 @@ from odoo import fields, models, Command
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
-    training_date = fields.DateTimeField(auto_now_add=True, string="Training Date")
+    training_date = fields.DateTime(auto_now_add=True, string="Training Date")
     employee = fields.Char(string="Assigned Employee")
     approval_status = fields.Selection(
     related='approval_id.state',
