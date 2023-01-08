2a2c599bffab3fe66927232741630fd834d35630
Alexandru Lazar <79793893+alazar-HELB@users.noreply.github.com>
2023-01-08 23:03:51 +0100

    part2

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -10,7 +10,7 @@ class SaleOrder(models.Model):
                 raise ValidationError(_('The sale order amount is greater than the maximum allowed amount for the partner.'))
 
     approval_status = fields.Selection(
-    related='approval_id.state',
+    related='approval_id.1',
     string='Approval Status',
     )
 
--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -4,7 +4,4 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     training_date = fields.datetime(year=2023, month=1, day= 1)
     employee = fields.Many2one(string="Assigned Employee")
-    approval_status = fields.Selection(
-    related='approval_id.state',
-    string='Approval Status',
-    )
+    approval_status = fields.Selection(string='Approval Status')
